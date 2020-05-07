f51de06f998c2c34baec05f780200ee5da8b9d34
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-05-07 17:13:28 +0300

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 python manage.py migrate
-python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput
 
 exec gunicorn -w 4 beyan_real_estates.wsgi:application --bind 0.0.0.0:8000
